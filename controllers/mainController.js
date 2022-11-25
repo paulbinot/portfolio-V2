422ad09232863a9def2d6141ce606535dd2e2b9e
paul BINOT <paulbinot23@gmail.com>
2022-11-25 21:33:43 +0100

    adding skills section content

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,10 @@
 // Require data here if needed
+const techSkills = require('../data/techSkills.json');
+const otherSkills = require('../data/otherSkills.json');
 
 const mainController = {
   renderHomePage: (req, res) => {
-    res.render("index");
+    res.render("index", { techSkills, otherSkills });
   }
 };
 
