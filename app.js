b72aab01e3770367bd12be9af77be7d23b259117
paul BINOT <paulbinot23@gmail.com>
2023-02-09 18:30:57 +0100

    add contact form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const nodemailer = require('nodemailer');
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -14,13 +15,39 @@ app.set("views", "./src/views");
 
 // -----MIDDLEWARES-----
 app.use(express.static("public"));
+app.use(express.json());
 
+// ----MAILER----
+app.post('/', (req, res) => {
+  console.log(req.body);
 
+  const transporter = nodemailer.createTransport({
+    service: process.env.MAIL_SERVICE,
+    secure: false,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.MAIL_PASSWORD,
+    }
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.MAIL,
+    subject: `Message de la part de ${req.body.name} (${req.body.email}) : ${req.body.subject}`,
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      console.log(error);
+      return res.send(error);
+    }
+    res.send("success");
+  });
+});
 
 // Routers
 app.use(mainRouter);
-
-
 
 // 404
 app.use((req, res) => {
---
+++ public/mailer.js
@@ -0,0 +1,33 @@
+const contactForm = document.getElementById('contact-form');
+const name = document.getElementById('contact-form-name');
+const email = document.getElementById('contact-form-email');
+const subject = document.getElementById('contact-form-subject');
+const message = document.getElementById('contact-form-message');
+
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value
+  };
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function() {
+    if (xhr.responseText === 'success') {
+      alert("email send");
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("Something went wrong");
+    }
+  };
+
+  xhr.send(JSON.stringify(formData));
+});
